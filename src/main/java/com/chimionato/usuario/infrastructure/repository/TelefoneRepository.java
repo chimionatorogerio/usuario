3427d1a82cd152f14376f417dac2ca12e9fb1797
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-10-10 15:38:09 -0300

    adicionados metodos get, delete e login

--- src/main/java/com/chimionato/usuario/infrastructure/repository/TelefoneRepository.java
+++ src/main/java/com/chimionato/usuario/infrastructure/repository/TelefoneRepository.java
@@ -1,6 +1,6 @@
 package com.chimionato.usuario.infrastructure.repository;
 
-import com.javanauta.aprendendospring.infrastructure.entity.Telefone;
+import com.chimionato.usuario.infrastructure.entity.Telefone;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
